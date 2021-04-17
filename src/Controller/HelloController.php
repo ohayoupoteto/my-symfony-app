800c286066a53092d8390735bcf3452daae66c33
佐藤　聖利 <ohayoupoteto20000202@ezweb.ne.jp>
2021-04-18 00:55:15 +0900

    二種類のリダイレクトまで

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -2,21 +2,38 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 
-class HelloController{
+class HelloController extends AbstractController{
     /** 
      * @Route("/hello", name="hello")
      */
-    public function index(){
+    public function index(Request $request){
+        $name = $request->get("name");
+        $name2 = $request->get("name2");
         $result = <<< EOM
         <ul>
-          <li>牛タン</li>
-          <li>ずんだ</li>
-          <li>笹かま</li>
+          <li>{$name}</li>
+          <li>{$name2}</li>
         </ul>
 EOM;
+        //$result = $this->getSubscribedServices();
         return new Response($result);
+    }
+
+    /**
+     * @Route("/other/{domain}",name="other")
+     */
+    public function other(Request $request ,$domain=""){
+        if($domain==""){
+            return $this->redirect("/hello");
+        }
+        else{
+            return new RedirectResponse("http://{$domain}.com");
+        }
     }
 }
