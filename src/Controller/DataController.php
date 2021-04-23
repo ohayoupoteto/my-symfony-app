d281f737608a895f7daac2395ef0ff18230faf5d
佐藤　聖利 <ohayoupoteto20000202@ezweb.ne.jp>
2021-04-23 21:05:26 +0900

    IDでエンティティを検索

---
+++ src/Controller/DataController.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
+use App\Entity\Person;
+
+class DataController extends AbstractController
+{
+    /**
+     * @Route("/find", name="find")
+     */
+    public function find(Request $request)
+    {
+        $formObj=new FindForm();
+
+        $form=$this->createFormBuilder($formObj)
+        ->add('find',TextType::class)
+        ->add('save',SubmitType::class)
+        ->getForm();
+
+        if($request->getMethod()=='POST'){
+            $form->handleRequest($request);
+            $data=$form->getData()->getFind();
+
+            $repository=$this->getDoctrine()->getRepository(Person::class);
+            $result=$repository->find($data);
+        }
+        else{
+            $result=null;
+        }
+
+        return $this->render('data/find.html.twig',[
+            'title'=>'IDでエンティティを検索する',
+            'data'=>$result,
+            'form'=>$form->createView(),
+        ]);
+    }
+}
+
+class FindForm{
+    private $find;
+    public function getFind(){
+        return $this->find;
+    }
+    public function setFind($find){
+        $this->find = $find;
+    }
+
+}
--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -187,3 +187,4 @@ class MyData{
         $this->data=$data;
     }
 }
+
